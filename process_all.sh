f3bfadbb651699e6ce185624b418e9d3858997ce
Manuel Raimann <raimannma@outlook.de>
2024-01-02 20:39:36 +0100

    improve bash script

--- process_all.sh
+++ process_all.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+trap stop SIGINT
+
+stop() {
+  echo "Stopping..."
+  rm -r clips/*;
+  rm -r matches/*;
+  docker-compose down
+  docker rm ghcr.io/raimannma/clip-cutter
+  exit 0
+}
+
 mkdir -p clips;
 rm -rf clips/*;
 
@@ -12,11 +23,11 @@ for channel in $(jq -r '.[] | .channel' users.json); do
   echo "riot_ids: $riot_ids"
   for video in $(twitch-dl videos "$channel" --all -t archive -j | jq '.videos' | jq -r '.[].id'); do
       echo "Processing $video";
-      docker-compose run --rm --entrypoint "clip-cutter -v $video -r '$riot_ids' --remove-matches" clip_cutter;
-      rclone move -P clips/ Nextcloud:ClipCutter/New/"$channel"/;
+      docker-compose run --rm --entrypoint "clip-cutter -v $video -r '$riot_ids' --remove-matches" clip_cutter || break;
+      rclone move -P clips/ Nextcloud:ClipCutter/New/"$channel"/ || break;
       rm -r clips/*;
+      rm -r matches/*;
   done
 done
 
-docker-compose down
-docker rm ghcr.io/raimannma/clip-cutter
+stop
