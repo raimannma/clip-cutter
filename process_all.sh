7ca354f73ec8bc77042700f7c92442fd4762c283
Manuel Raimann <raimannma@outlook.de>
2024-01-02 21:39:13 +0100

    Push to main dir

--- process_all.sh
+++ process_all.sh
@@ -27,7 +27,7 @@ for channel in $(jq -r '.[] | .channel' users.json); do
   for video in $(twitch-dl videos "$channel" --all -t archive -j | jq '.videos' | jq -r '.[].id'); do
       echo "Processing $video";
       docker-compose run --rm --entrypoint "clip-cutter -v $video -r '$riot_ids' --remove-matches" clip_cutter || break;
-      rclone move -P clips/ Nextcloud:ClipCutter/New/"$channel"/ || break;
+      rclone move -P clips/ Nextcloud:ClipCutter/"$channel"/ || break;
       rm -r clips/*;
       rm -r matches/*;
   done
