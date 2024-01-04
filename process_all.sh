9c0ba086529f8d56d411a6c6cea2a1cfbdebae01
Manuel Raimann <raimannma@outlook.de>
2024-01-04 09:29:32 +0100

    Only process videos from last 2 days

--- process_all.sh
+++ process_all.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+source /root/.bashrc
 
 docker-compose up -d redis --wait;
 
@@ -24,7 +26,7 @@ for channel in $(jq -r '.[] | .channel' users.json); do
   echo "Processing $channel"
   riot_ids=$(jq -r ".[] | select(.channel == \"$channel\") | .riot_ids | join(\",\")" users.json)
   echo "riot_ids: $riot_ids"
-  for video in $(twitch-dl videos "$channel" --all -t archive -j | jq '.videos' | jq -r '.[].id'); do
+  for video in $(twitch-dl videos "$channel" --all -t archive -j | jq -r '.videos | .[] | select((now - (.publishedAt | fromdateiso8601)) < (2 * 24 * 3600)) | .id'); do
       clean
       echo "Processing $video";
       docker-compose run --rm --entrypoint "clip-cutter -v $video -r '$riot_ids' --remove-matches" clip_cutter || continue;
