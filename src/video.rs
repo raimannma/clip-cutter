12b320589247365c081044db7ad0cdf6520f41f6
Manuel Raimann <raimannma@outlook.de>
2024-01-12 11:52:22 +0100

    Fix video splitting

--- src/video.rs
+++ src/video.rs
@@ -89,7 +89,7 @@ pub(crate) fn split_video(
         None => "".to_string(),
     };
     let ffmpeg_split_command = format!(
-        "ffmpeg -y -i {} -ss {} -to {} -c:a copy -c:v {} -copyts {} {}",
+        "ffmpeg -y -ss {} -to {} -i {} -c:a copy -c:v {} -copyts {} {}",
         path.to_str().unwrap(),
         format_ffmpeg_time(start, true),
         format_ffmpeg_time(end, true),
