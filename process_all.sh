fd243b17fed96a8d1f654dc35ad3db22e834d6c7
Manuel Raimann <raimannma@outlook.de>
2024-02-19 15:35:08 +0100

    Update process_all script

--- process_all.sh
+++ process_all.sh
@@ -4,6 +4,8 @@ source /root/.bashrc
 
 trap stop SIGINT
 
+docker pull ghcr.io/raimannma/clip-cutter:latest
+
 clean() {
   echo "Cleaning..."
   mkdir -p clips;
@@ -15,7 +17,6 @@ clean() {
 
 stop() {
   echo "Stopping..."
-  docker image rm ghcr.io/raimannma/clip-cutter
   exit 0
 }
 
@@ -23,13 +24,15 @@ for channel in $(jq -r '.[] | .channel' users.json); do
   echo "Processing $channel"
   riot_ids=$(jq -r ".[] | select(.channel == \"$channel\") | .riot_ids | join(\",\")" users.json)
   echo "riot_ids: $riot_ids"
-  for video in $(twitch-dl videos "$channel" --all -t archive -j | jq -r '.videos | .[] | select((now - (.publishedAt | fromdateiso8601)) < (2 * 24 * 3600)) | .id'); do
+  for video in $(twitch-dl videos "$channel" --all -t archive -j | jq -r '.videos | .[] | select((now - (.publishedAt | fromdateiso8601)) < (3 * 24 * 3600)) | .id'); do
       clean
       echo "Processing $video";
-      docker-compose run --rm --entrypoint "clip-cutter -v $video -r '$riot_ids' --remove-matches" clip_cutter || continue;
-      rclone move -P clips/ Nextcloud:ClipCutter/"$channel"/ || continue;
+      docker compose run --rm --entrypoint "clip-cutter -v $video -r '$riot_ids' --remove-matches" clip_cutter || continue;
+      rclone move --ignore-existing -P clips/ Nextcloud:ClipCutter/"$channel"/ || continue;
   done
 done
+
+curl "https://status.manuel-hexe.de/api/push/VXB87TfmjS?status=up&msg=OK&ping=" || true
 
 clean
 stop
